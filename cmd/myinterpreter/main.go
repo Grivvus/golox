288d5d79caa4ef585f16a97406a26f363ab652c1
Arseniy Krupchik <kruchik.a04@gmail.com>
2025-07-30 13:02:26 +0400

    little cleanup

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they"ll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
 		os.Exit(1)
@@ -21,8 +18,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
-
-	// Uncomment this block to pass the first stage
 
 	filename := os.Args[2]
 	fileContents, err := os.ReadFile(filename)
